40f69a56c8c0833f36ce6f93e7e935bb43af08e8
우승화(4) <u60192218@sysdesignlab3.mju.ac.kr>
2024-11-05 10:11:12 +0900

    feat: Reactor

--- chat_server/src/Reactor.cpp
+++ chat_server/src/Reactor.cpp
@@ -1,10 +1,13 @@
 #include "Reactor.h"
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+
 #include <iostream>
 #include <unistd.h>
 #include <string.h>
 #include <error.h>
+#include <set>
 
 using namespace std;
 
@@ -41,13 +44,39 @@ void Reactor::init(){
 
 /// @brief Server 실행
 void Reactor::run(){
-
     // 정상적으로 서버 연결이 완료된 경우
     cout << "Server Started..." << endl;
     
+    fd_set readFd;
+    int maxFd = 0;
+
+    set<int> clientSocks;
+
     while (true)
     {
         // todo : I/O multiplexing으로 연결 받기
-    }
+        FD_ZERO(&readFd);
+        FD_SET(serverSock, &readFd);
+        maxFd = serverSock;
+
+        for (int clientSock : clientSocks) {
+            FD_SET(clientSock, &readFd);
+            if (clientSock > maxFd) {
+                maxFd = clientSock;
+            }
+        }
+
+        int numReady = select(maxFd + 1, &readFd, NULL, NULL, NULL);
+            if (numReady < 0) {
+                cerr << "select() failed: " << strerror(errno) << endl;
+                continue;
+            } else if (numReady == 0) {
+                continue;
+            }
+        }
+
+        if(FD_ISSET(serverSock, &readFd)){
+            
+        }
 
 }
